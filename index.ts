3e700404b6ebacc11ddf22418105e6927da42599
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-08 13:08:37 -0800

    init drizzle

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { drizzle } from "drizzle-orm/libsql";
+import "dotenv/config";
 
 const PORT = 3000;
 const app = express();
+const db = drizzle(process.env.DATABASE_URL!);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}: http//localhost:3000`);
