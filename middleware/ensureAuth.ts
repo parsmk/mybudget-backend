decfa46453af19a1a56cca37c6ff1201e001aab1
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-12 15:43:46 -0800

    refactor: ensureAuth to just check accessToken

--- middleware/ensureAuth.ts
+++ middleware/ensureAuth.ts
@@ -3,13 +3,9 @@ import { verifyToken } from "../utils/auth";
 
 export const ensureAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const accessToken = req.cookies["accessToken"];
-    const refreshToken = req.cookies["refreshToken"];
-
-    if (!(accessToken && refreshToken)) return res.sendStatus(401);
-
+    const accessToken = req.cookies?.accessToken;
+    if (!accessToken) return res.sendStatus(401);
     verifyToken("access", accessToken);
-    verifyToken("refresh", refreshToken);
     return next();
   } catch (err) {
     return res.sendStatus(401);
