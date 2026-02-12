f70a39f0f5fd51e208049a917ddcd8fc84f633e8
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-12 15:17:34 -0800

    refactor: cleanup index.ts

--- index.ts
+++ index.ts
@@ -3,29 +3,26 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import { authRouter } from "./routes/authRoutes";
+import { transactionRouter } from "./routes/transactionRoutes";
+import { logEndpoint } from "./middleware/logEndpoint";
 
 // Config
 const PORT = 5000;
 const app = express();
+const CORS = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-);
-app.use((req, res, next) => {
-  res.on("finish", () => {
-    console.log(`${res.statusCode}: ${req.url}`);
-  });
-  next();
-});
+app.use(cors(CORS));
+app.use(logEndpoint);
 
 // Routes
 app.use("/", authRouter);
+app.use("/transaction/", transactionRouter);
 
 // Start Server
 app.listen(PORT, () => {
