9ed72ac1000c778832b3f6dc5c86209e597ae26d
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-12 17:38:35 -0800

    feat: getTransactions endpoint

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ensureAuth } from "../middleware/ensureAuth";
 import { db } from "../db";
 import { transactionSchema } from "../models/transaction";
+import { eq } from "drizzle-orm";
 
 export const transactionRouter = Router();
 
@@ -9,21 +10,37 @@ transactionRouter.post("/create", ensureAuth, async (req, res) => {
   try {
     const payload = req.body;
     const user = req.auth;
+    if (!user) return;
 
-    const transactions = payload.map((t: any) => ({ ...t, userID: user?.id }));
+    const transactions = payload.map((t: any) => ({ ...t, userID: user.id }));
 
     const uploaded = await db
       .insert(transactionSchema)
       .values(transactions)
       .returning();
 
-    return res.status(201).json({ transactions: uploaded });
+    return res.status(201).json(uploaded);
   } catch (error) {
     console.error(error);
     return res
       .status(500)
-      .json({ create_transactions: "Internal Server error!" });
+      .json({ create_transactions: "Internal server error!" });
   }
 });
 
-transactionRouter.get("/", ensureAuth, async (req, res) => {});
+transactionRouter.get("/", ensureAuth, async (req, res) => {
+  try {
+    const user = req.auth;
+    if (!user) return;
+
+    const transactions = await db
+      .select()
+      .from(transactionSchema)
+      .where(eq(transactionSchema.userID, user.id));
+
+    return res.status(200).json(transactions);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ get_transactions: "Internal server error!" });
+  }
+});
