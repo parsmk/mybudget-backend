b1e518e84d120e8d30e0507d03f2d64f063e8cdd
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-13 12:07:42 -0800

    feat: category endpoints

---
+++ src/routes/categoryRoutes.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { ensureAuth } from "../middleware/ensureAuth";
+import { db } from "../db";
+import { categorySchema } from "../models/transaction";
+import { eq } from "drizzle-orm";
+
+export const categoryRouter = Router();
+
+categoryRouter.post("/", ensureAuth, async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const newCategory = await db
+      .insert(categorySchema)
+      .values({ name: name, userID: req.auth?.id! })
+      .returning();
+
+    return res.status(204).json(newCategory);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal error" });
+  }
+});
+
+categoryRouter.get("/", ensureAuth, async (req, res) => {
+  try {
+    const categories = await db
+      .select()
+      .from(categorySchema)
+      .where(eq(categorySchema.userID, req.auth?.id!));
+
+    return res.status(200).json(categories);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal error" });
+  }
+});
