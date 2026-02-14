61c140c47959b211756179c72c6ea12ef225a5bd
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-14 14:51:46 -0800

    feat: UD endpoints for transactions

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { ensureAuth } from "../middleware/ensureAuth";
 import { db } from "../db";
 import { transactionSchema } from "../models/transaction";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 export const transactionRouter = Router();
 
@@ -22,7 +22,7 @@ transactionRouter.post("/", ensureAuth, async (req, res) => {
     return res.status(201).json(uploaded);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Internal server error!" });
+    return res.status(500).json({ error: "Internal error" });
   }
 });
 
@@ -36,6 +36,60 @@ transactionRouter.get("/", ensureAuth, async (req, res) => {
     return res.status(200).json(transactions);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Internal server error!" });
+    return res.status(500).json({ error: "Internal error" });
+  }
+});
+
+transactionRouter.patch<{ transactionId: string }>(
+  "/:transactionId",
+  ensureAuth,
+  async (req, res) => {
+    try {
+      const transactionId = req.params.transactionId;
+      const { id, userID, ...rest } = req.body;
+
+      const transaction = await db
+        .update(transactionSchema)
+        .set(rest)
+        .where(
+          and(
+            eq(transactionSchema.id, transactionId),
+            eq(transactionSchema.userID, req.auth?.id!),
+          ),
+        )
+        .returning();
+
+      if (transaction.length < 1)
+        return res.status(404).json({ error: "Could not find transaction" });
+
+      return res.status(200).json(transaction[0]);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal error" });
+    }
+  },
+);
+
+transactionRouter.delete<{ id: string }>("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const transaction = await db
+      .delete(transactionSchema)
+      .where(
+        and(
+          eq(transactionSchema.id, id),
+          eq(transactionSchema.userID, req.auth?.id!),
+        ),
+      )
+      .returning();
+
+    if (transaction.length < 1)
+      return res.status(404).json({ error: "Could not find transaction" });
+
+    return res.status(200).json(transaction[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal error" });
   }
 });
