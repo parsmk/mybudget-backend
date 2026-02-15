4d38ed60b7e6ea383aefd4869cc14750c1ba5ae1
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-15 11:38:00 -0800

    feat: account routes

---
+++ src/routes/accountRoutes.ts
@@ -0,0 +1,96 @@
+import { Router } from "express";
+import { ensureAuth } from "../middleware/ensureAuth";
+import { db } from "../db";
+import { accountSchema } from "../models/account";
+import { and, eq } from "drizzle-orm";
+
+export const accountRouter = Router();
+
+accountRouter.post("/", ensureAuth, async (req, res) => {
+  try {
+    const payload = req.body;
+
+    const account = await db
+      .insert(accountSchema)
+      .values({ ...payload, userID: req.auth?.id! })
+      .returning();
+
+    return res.status(200).json(account[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal error" });
+  }
+});
+
+accountRouter.get("/", ensureAuth, async (req, res) => {
+  try {
+    const accounts = await db
+      .select()
+      .from(accountSchema)
+      .where(eq(accountSchema.userID, req.auth?.id!));
+
+    return res.status(200).json(accounts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal error" });
+  }
+});
+
+accountRouter.get<{ id: string }>("/:id", ensureAuth, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const account = await db
+      .select()
+      .from(accountSchema)
+      .where(
+        and(eq(accountSchema.userID, req.auth?.id!), eq(accountSchema.id, id)),
+      );
+
+    if (account.length < 1)
+      return res.status(404).json({ error: "Account not found" });
+
+    return res.status(200).json(account[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal error" });
+  }
+});
+
+accountRouter.patch<{ id: string }>("/:id", ensureAuth, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const payload = req.body;
+
+    const account = await db
+      .update(accountSchema)
+      .set(payload)
+      .where(
+        and(eq(accountSchema.userID, req.auth?.id!), eq(accountSchema.id, id)),
+      )
+      .returning();
+
+    return res.status(200).json(account[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal error" });
+  }
+});
+
+accountRouter.delete<{ id: string }>("/:id", ensureAuth, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const account = await db
+      .delete(accountSchema)
+      .where(
+        and(eq(accountSchema.userID, req.auth?.id!), eq(accountSchema.id, id)),
+      )
+      .returning();
+
+    return res.status(200).json(account);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal error" });
+  }
+});
